a5aef828908f85dc05c9ada61fdda4bd5a8d8171
D-Farouk <faroukdell@hotmail.com>
2024-02-29 16:55:25 +0100

    Ajout des pop up pour Inscription et Connexion afin que ca reste dans la page d'accueil

---
+++ client/src/components/Auth/InscriptionC.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import {useForm} from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { sendAuthMail } from "../SendMailConfirm";
+import { initial } from "../Initiale";
+
+// notre page d'inscription, qui ressemble beaucoup a celle du Login.
+const InscriptionC = (props) => {
+
+    const navigate = useNavigate();
+
+    // Watch ici permet de vérifier une valeur de notre register
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: {errors},
+    } = useForm()
+
+    // Fonction du submit de l'inscription qui envoie un mail avec un code généré aléatoirement au mail de l'inscription et qui doit le vérifier pour s'inscrire 
+    const onSubmit = async (data) => {
+        initial()
+        try {
+        const code = Math.floor(Math.random() * 1000000);
+        await sendAuthMail(data.email,code);
+        navigate("/verifemail", { state: { code, pseudonyme: data.pseudonyme } } );
+        } catch(err) {
+            console.log(err)
+        }
+    }
+    return (props.trigger) ? (
+        <div className="fixed inset-0 flex items-center justify-center ">
+        
+        <div className="flex flex-col w-full items-center justify-center mx-auto max-w-md md:h-screen lg:py-0">
+            <a className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-[#e0c758]">
+                Inscription
+            </a>
+            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 xl:p-0 dark:bg-[#0c0c0c] dark:border-[#e0c758]">
+                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              
+              <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit(onSubmit)} >
+                  <div>
+                      <label htmlFor="pseudonyme" className="block mb-2 text-sm font-medium text-gray-900 dark:text-[#e0c758]">Pseudonyme</label>
+                      <input type="text" name="pseudonyme" id="pseudonyme"
+                      aria-invalid={errors.name ? "true" : "false"}
+                      {...register("pseudonyme",{required: true,maxLength:30})} 
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Identifiant"/>
+                   {errors.pseudonyme && errors.pseudonyme.type === "required" && (
+                            <span role="alert" className="text-red-500 text-sm ">Pseudonyme Requis</span>
+                        )}
+                  </div>
+                  <div>
+                      <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-[#e0c758]">Email</label>
+                      <input type="text" name="email" id="email" 
+                      {...register("email",{required: true,maxLength:30})} placeholder="nom.prenom@email.com" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 "/>
+                    {errors.email && errors.email.type === "required" && (
+                            <span role="alert" className="text-red-500 text-sm ">Email requis</span>
+                        )}
+                  </div>
+                  <div>
+                      <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-[#e0c758]">Mot de Passe</label>
+                      <input type="password" name="password" id="password" 
+                      {...register("password",{required: true,maxLength:30})} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                  {errors.password && errors.password.type === "required" && (
+                            <span role="alert" className="text-red-500 text-sm ">Mot de Passe requis</span>
+                        )}
+                  </div>
+                  <div>
+                      <label htmlFor="confirmpassword" className="block mb-2 text-sm font-medium text-gray-900 dark:text-[#e0c758]">Confirmer Mot de Passe</label>
+                      <input type="password" name="confirmpassword" 
+                      {...register("confirmpassword",{required: true,maxLength:30})} id="Confirmpassword" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                  {errors.confirmpassword && errors.confirmpassword.type === "required" && (
+                            <span role="alert" className="text-red-500 text-sm ">Confirmation du mot de passe requis</span>
+                        )}
+                     {!errors.confirmpassword && watch("password") !== watch("confirmpassword") && (
+                            <span role="alert" className="text-red-500 text-sm ">Les mots de passe ne correspondent pas</span>
+                        )}
+                  </div>
+                  <div className="flex items-center justify-between">
+                      <div className="flex items-start">
+                          <div className="flex items-center h-5">
+                            <input id="remember" aria-describedby="remember" type="checkbox"
+                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
+                          </div>
+                          <div className="ml-3 text-sm">
+                            <label htmlFor="remember" className="text-gray-500 dark:text-[#e0c758]">Se souvenir de moi</label>
+                          </div>
+                      </div>
+                  </div>
+                  <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 dark:text-[#e0c758]" >Inscription</button>
+              </form>
+          </div>
+      </div>
+  </div>
+  </div>
+    ) : "";
+}
+
+export default InscriptionC
--- client/src/components/NavigBar.jsx
+++ client/src/components/NavigBar.jsx
@@ -1,10 +1,13 @@
 import React, {useState,useEffect } from "react";
 import { ImMenu,ImMenu3  } from "react-icons/im";
+import ConnexionC from "./Auth/ConnexionC";
 
 // Composant permettant d'afficher la barre de navigation dans notre page
 const NavigBar = () => {
 
     const [bar,setBar] = useState(true)
+
+    const [loginOpen,setLoginOpen] = useState(false)
 
     const [isAuthenticated, setIsAuthenticated] = useState(false);
 
@@ -27,7 +30,12 @@ const NavigBar = () => {
     const handleBar = () => {
         setBar(!bar)
     }
+
+    const handleLogin = () => {
+        setLoginOpen(!loginOpen)
+    }
     return(
+<>
         <div className="flex justify-between items-center h-24  mx-auto px-4 text-white bg-[#181717]" >
             <button onClick={handleBar} className="absolute top-8.5 ">
               {!bar ? <ImMenu3  size={33} /> : <ImMenu  size={25} />}
@@ -36,10 +44,7 @@ const NavigBar = () => {
                 e.preventDefault();
                 window.location.href='/';
                 }}>Mini Games</button></div>
-            {!isAuthenticated ? <button className="absolute right-12 text-[#f0efea] hover:text-[#e0c758] bg-[#181717] w-[100px] rounded-md text-[85%] font-medium my-6 mx-auto py-2 px-3  hover:scale-105 duration-300" onClick={(e) => {
-                e.preventDefault();
-                window.location.href='/Connexion';
-                }}>Connexion
+            {!isAuthenticated ? <button className="absolute right-12 text-[#f0efea] hover:text-[#e0c758] bg-[#181717] w-[100px] rounded-md text-[85%] font-medium my-6 mx-auto py-2 px-3  hover:scale-105 duration-300" onClick={handleLogin}>Connexion
             </button>:<button className="absolute right-12 text-[#f0efea] hover:text-[#e0c758] bg-[#181717] w-[100px] rounded-md text-[85%] font-medium my-6 mx-auto py-2 px-3  hover:scale-105 duration-300" 
             onClick={(e) => {
                 e.preventDefault();
@@ -56,7 +61,23 @@ const NavigBar = () => {
                 </ul> 
             </div>
         </div>
+        {loginOpen && <div className="fixed inset-0 flex items-center justify-center z-50 bg-gray-800 bg-opacity-50">
+                <div className="w-full p-8 rounded-lg">
+                    <ConnexionC trigger={loginOpen}/>
+                    <button onClick={handleLogin} className="absolute left-[60%] top-[18%] transform -translate-x-1/2 text-[#e0c758] hover:text-[#494222]">
+                        Fermer
+                    </button>
+                </div>
+            </div>}
+        </>
+        
+
+
     )
 }
 
 export default NavigBar
+
+// {/* <button onClick={handleLogin} className="absolute left-[60%] top-[18.5%] transform -translate-x-1/2 text-[#e0c758] hover:text-[#494222]">
+//                         Fermer
+//                     </button> */}
