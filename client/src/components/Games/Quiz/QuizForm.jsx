0d0b29247dd1f49079ebedf0631d6dea4f5f1ed4
D-Farouk <faroukdell@hotmail.com>
2024-03-24 03:15:40 +0100

    Amélioration de la page du jeu QUIZ (progressBar + gestion d'un store Quiz +  responsive)

--- client/src/components/Games/Quiz/QuizForm.jsx
+++ client/src/components/Games/Quiz/QuizForm.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import { useForm } from "react-hook-form"
 import { useQuizStore } from "../../../store"
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+import { styled } from '@mui/material/styles';
 
 const QuizForm = () => {
     const {
@@ -9,10 +11,25 @@ const QuizForm = () => {
         formState: {errors},
     } = useForm()
 
+    const [progressValue,setProgressValue] = useState(20)
+    
+    
+    const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+        height: 10,
+        borderRadius: 5,
+        [`&.${linearProgressClasses.colorPrimary}`]: {
+          backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+        },
+        [`& .${linearProgressClasses.bar}`]: {
+          borderRadius: 5,
+          backgroundColor: theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8',
+        },
+      }));
+
     const theme = useQuizStore((state) => state.theme)
 
     const onSubmit =  (data) => {
-        setNumber(number+1)
+        if (progressValue != 100) {setProgressValue(progressValue+20)}
     }
 
     const [number,setNumber] = useState(1)
@@ -24,56 +41,52 @@ const QuizForm = () => {
         <div className="flex flex-col w-full items-center justify-center mx-auto sm:max-w-md  lg:py-0 ">
         
             <div className="w-full bg-white sm:rounded-lg shadow dark:border  xl:p-0 dark:bg-[#FFFFFF] dark:bg-opacity-50 dark:border-transparent h-full sm:h-auto mt-[95px]">
+                <h2 className="font-bold px-3 py-5 text-xl sm:text-xl text-[#070707] text-shadow">Thème : {theme}</h2>
+                <p className="font-bold px-3 py-5 text-xl sm:text-xl text-[#070707] text-shadow">Question {progressValue / 20 } / 5 </p>
+                <div className="px-5 w-[80%]"><BorderLinearProgress variant="determinate" value={progressValue} /></div>
                 <div className="p-6 space-y-4 md:space-y-6 sm:p-8 relative ">
+                    
                 <div 
                     className="absolute top-6 right-6  flex  justify-center  translate-x-1/2 -translate-y-1/2 ">
-                         <img src="/images/HibouQuizWiz.png" alt="bug" className="w-1/3 cursor-pointer mb-10 md:mb-0 hidden sm:block "/>
+                         <img src="/images/HibouQuizWiz.png" alt="bug" className="w-1/3 cursor-pointer hidden sm:block "/>
                 </div>
               
               <form className="space-y-4 md:space-y-6  " action="#" onSubmit={handleSubmit(onSubmit)} >
-                  <h2 className="font-bold text-xl sm:text-2xl text-[#070707] text-shadow">{theme}</h2>
+                  
                   <div>
                       <input type="text" name="pseudonyme" id="pseudonyme"
-                      aria-invalid={errors.name ? "true" : "false"}
-                      {...register("pseudonyme",{required: true,maxLength:30})} 
+                  
+                      {...register("pseudonyme",{maxLength:30})} 
                       className="bg-gray-50 border border-gray-300 text-[#313030] sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-[#FFFFFF] dark:border-gray-600 dark:placeholder-[#474444] dark:text-[#474444] dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Identifiant"/>
                 
                   </div>
                   <div>
                       
                       <input type="text" name="email" id="email" 
-                      {...register("email",{required: true,maxLength:30})} placeholder="Email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-[#FFFFFF] dark:border-gray-600 dark:placeholder-[#474444] dark:text-[#474444] dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                      {...register("email",{maxLength:30})} placeholder="Email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-[#FFFFFF] dark:border-gray-600 dark:placeholder-[#474444] dark:text-[#474444] dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                     
                   </div>
                   <div>
                       
                       <input type="password" name="password" id="password" 
-                      {...register("password",{required: true,maxLength:30})} placeholder="Mot de passe" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-[#FFFFFF] dark:border-gray-600 dark:placeholder-[#474444] dark:text-[#474444] dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                      {...register("password",{maxLength:30})} placeholder="Mot de passe" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-[#FFFFFF] dark:border-gray-600 dark:placeholder-[#474444] dark:text-[#474444] dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                   
                   </div>
                   <div>
                       
                       <input type="password" name="confirmpassword" 
-                      {...register("confirmpassword",{required: true,maxLength:30})} id="Confirmpassword" placeholder="Confirmation du mot de passe" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-[#FFFFFF] dark:border-gray-600 dark:placeholder-[#474444] dark:text-[#474444] dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                      {...register("confirmpassword",{maxLength:30})} id="Confirmpassword" placeholder="Confirmation du mot de passe" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-[#FFFFFF] dark:border-gray-600 dark:placeholder-[#474444] dark:text-[#474444] dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                  
                      
                   </div>
                   <div className="flex items-center justify-between">
                       <div className="flex items-start">
-                          <div className="flex items-center h-5">
-                            <input id="remember" aria-describedby="remember" type="checkbox"
-                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
-                          </div>
-                          <div className="ml-3 text-sm">
-                            <label htmlFor="remember" className="text-gray-500 dark:text-[#fcfafa]">Se souvenir de moi</label>
-                          </div>
+                          
                       </div>
                   </div>
-                  <p className="text-sm font-light text-gray-500  dark:text-[#fffffe]">
-                      Déja inscris ? <a className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500 dark:text-[#fffefc]"  >Cliquez ici</a>
-                  </p>
+                  
                   <button type="submit"    className="w-full text-white bg-[#2f9421] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 dark:text-[#eeeeed] hover:scale-105 duration-300
-                  hover:bg-[#245e2c]"  >Inscription</button>
+                  hover:bg-[#245e2c]"  >Soumettre </button>
               </form>
           
           </div>
--- client/src/components/Games/Quiz/ThemeChoice.jsx
+++ client/src/components/Games/Quiz/ThemeChoice.jsx
@@ -20,14 +20,14 @@ const GamesChoice = () => {
             Choisis ton Quiz
           </p>
           <div className="absolute  left-3 top-16 h-full w-full m-4 items-center justify-center py-16 px-10 flex flex-wrap overflow-y-scroll">
-            <img src="/images/Themes/histoire.png" alt="bug" className="w-1/2 md:w-[13%] h-auto rounded-2xl hover:scale-105 duration-300 mr-12 cursor-pointer mb-10 md:mb-0" onClick={() => onClick("histoire")}/>
-            <img src="/images/Themes/musique.png" alt="bug" className="w-1/2 md:w-[13%] h-auto rounded-2xl hover:scale-105 duration-300 mr-12 cursor-pointer mb-10 md:mb-0" onClick={() => onClick("musique")}/>
-            <img src="/images/Themes/culture_generale.png" alt="bug" className="w-1/2 md:w-[13%] h-auto rounded-2xl hover:scale-105 duration-300 mr-12 cursor-pointer mb-10 md:mb-0" onClick={() => onClick("culture générale")}/>
-            <img src="/images/Themes/geographie.png" alt="bug" className="w-1/2 md:w-[13%] h-auto rounded-2xl hover:scale-105 duration-300  mr-12 cursor-pointer mb-10 md:mb-0" onClick={() => onClick("géographie")}/>
-            <img src="/images/Themes/animaux.png" alt="bug" className="w-1/2 md:w-[13%] h-auto rounded-2xl hover:scale-105 duration-300 mr-12 cursor-pointer mb-10 md:mb-0" onClick={() => onClick("animaux")}/>
-            <img src="/images/Themes/celebrites.png" alt="bug" className="w-1/2 md:w-[13%] h-auto rounded-2xl hover:scale-105 duration-300 mr-12 cursor-pointer mb-10 md:mb-0" onClick={() => onClick("célebrités")}/>
-            <img src="/images/Themes/cinema.png" alt="bug" className="w-1/2 md:w-[13%] h-auto rounded-2xl hover:scale-105 duration-300 mr-12 cursor-pointer mb-10 md:mb-0"onClick={() => onClick("cinema")}/>
-            <img src="/images/Themes/informatique.png" alt="bug" className="w-1/2 md:w-[13%] h-auto rounded-2xl hover:scale-105 duration-300 mr-12 cursor-pointer mb-10 md:mb-0" onClick={() => onClick("informatique")}/>
+            <img src="/images/Themes/histoire.png" alt="bug" className="w-1/2 md:w-[13%] h-auto rounded-2xl hover:scale-105 duration-300 mr-12 cursor-pointer mb-10 md:mb-0" onClick={() => onClick("Histoire")}/>
+            <img src="/images/Themes/musique.png" alt="bug" className="w-1/2 md:w-[13%] h-auto rounded-2xl hover:scale-105 duration-300 mr-12 cursor-pointer mb-10 md:mb-0" onClick={() => onClick("Musique")}/>
+            <img src="/images/Themes/culture_generale.png" alt="bug" className="w-1/2 md:w-[13%] h-auto rounded-2xl hover:scale-105 duration-300 mr-12 cursor-pointer mb-10 md:mb-0" onClick={() => onClick("Culture Générale")}/>
+            <img src="/images/Themes/geographie.png" alt="bug" className="w-1/2 md:w-[13%] h-auto rounded-2xl hover:scale-105 duration-300  mr-12 cursor-pointer mb-10 md:mb-0" onClick={() => onClick("Géographie")}/>
+            <img src="/images/Themes/animaux.png" alt="bug" className="w-1/2 md:w-[13%] h-auto rounded-2xl hover:scale-105 duration-300 mr-12 cursor-pointer mb-10 md:mb-0" onClick={() => onClick("Animaux")}/>
+            <img src="/images/Themes/celebrites.png" alt="bug" className="w-1/2 md:w-[13%] h-auto rounded-2xl hover:scale-105 duration-300 mr-12 cursor-pointer mb-10 md:mb-0" onClick={() => onClick("Célebrités")}/>
+            <img src="/images/Themes/cinema.png" alt="bug" className="w-1/2 md:w-[13%] h-auto rounded-2xl hover:scale-105 duration-300 mr-12 cursor-pointer mb-10 md:mb-0"onClick={() => onClick("Cinema")}/>
+            <img src="/images/Themes/informatique.png" alt="bug" className="w-1/2 md:w-[13%] h-auto rounded-2xl hover:scale-105 duration-300 mr-12 cursor-pointer mb-10 md:mb-0" onClick={() => onClick("Informatique")}/>
          
           
         </div>
