5737bbd83cd869b2590ec29299edec8af1e814e8
ciscom224 <mamadouciss224e@gmail.com>
2024-04-11 16:19:40 +0200

    bien

--- client/src/pages/Error.page.jsx
+++ client/src/pages/Error.page.jsx
@@ -1,15 +1,11 @@
-import React,{useState,useEffect} from 'react';
+import React from 'react';
 
-// Cette fonction represente notre page principale et fais appelle aux composants necessaire 
-function Error() {
-  
-
-  return (
-    
-      <div className="w-full h-screen bg-cover bg-center overflow-y-auto" style={{backgroundImage: "url('/images/Background/games_bg.png')"}}>
-        <h2>error page</h2>
-      </div>
-  )
+function Error(props) {
+    return (
+        <div>
+            Error page
+        </div>
+    );
 }
 
 export default Error;
