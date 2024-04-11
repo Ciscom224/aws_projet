e57fe8974caddc95d992c9a56523a291f5531e45
ciscom224 <mamadouciss224e@gmail.com>
2024-04-11 16:39:19 +0200

    mise a jour

--- client/src/pages/Home.page.jsx
+++ client/src/pages/Home.page.jsx
@@ -1,6 +1,5 @@
-import React,{useState,useEffect,useContext} from 'react';
+import React,{useContext} from 'react';
 import { UidContext } from "../AppContext";
-import NavBar from '../components/NavBar.component';
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
