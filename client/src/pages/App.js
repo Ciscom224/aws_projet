ec0bf4e442db7827cf769f28cb41404a38eaf00a
ciscom224 <mamadouciss224e@gmail.com>
2024-05-04 18:10:20 +0200

    error

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -5,19 +5,17 @@ import { UidContext } from '../AppContext';
 import { useDispatch } from 'react-redux';
 
 import { getUser } from '../actions/user.actions';
+import { getQuiz } from '../actions/quiz.actions';
 import Home from './Home.page';
 import Games from './Games';
 import QuizChoice from './QuizChoice';
 import NavBar from '../components/NavBar.component';
 import Error from './Error.page';
 import Admin from './admin/home.admin.page';
-import Classement from './Classement.jsx';
 
 function App() {
   const dispatch = useDispatch()
   const [uid, setUid] = useState(null)
-  const [loginOpen,setLoginOpen] = useState(false)
-
 
   useEffect(() => {
     async function checkAuth() {
@@ -34,7 +32,11 @@ function App() {
     }
     checkAuth()
 
-    if (uid) dispatch(getUser(uid));
+    if (uid) {
+      dispatch(getUser(uid));
+      dispatch(getQuiz())
+      
+    }
 
   }, [uid])
 
@@ -42,12 +44,11 @@ function App() {
     <UidContext.Provider value={uid}>
       <Router>
         <div className="w-full h-screen bg-cover  bg-center overflow-hidden " style={{ backgroundImage: "url('/images/Background/menu_bg.jpg')" }}>
-          <NavBar setLoginOpen={setLoginOpen} loginOpen={loginOpen}/>
+          <NavBar/>
           <main className='h-screen '>
             <Routes>
-            <Route path="/" element={<Home setLoginOpen={setLoginOpen} loginOpen={loginOpen}/>} />
+            <Route path="/" element={<Home />} />
               <Route path="/admin" element={<Admin/>} />
-              <Route path="/classement" element={uid ? <Classement/>: <Navigate to="/" />} />
               <Route path="/games" element={ uid ? <Games />:<Navigate to="/" />}/>
               <Route path="/games/quizchoice" element={ uid ? <QuizChoice />: <Navigate to="/"/>} />
               <Route path="*" element={<Error/>} />
